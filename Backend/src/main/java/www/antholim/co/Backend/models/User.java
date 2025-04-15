93088895f1e9c19b9538f09f4015c709139903f0
antholim <139510082+antholim@users.noreply.github.com>
2025-04-14 23:41:34 -0400

    user service

--- Backend/src/main/java/www/antholim/co/Backend/models/User.java
+++ Backend/src/main/java/www/antholim/co/Backend/models/User.java
@@ -1,11 +1,18 @@
 package www.antholim.co.Backend.models;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.lang.annotation.Documented;
 import java.util.Collection;
 import java.util.List;
 
+@Accessors(chain = true)
+@Setter
+@Getter
 public class User implements UserDetails {
     private String _id;
     private String username;
---
+++ Backend/src/main/java/www/antholim/co/Backend/services/implementations/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package www.antholim.co.Backend.services.implementations;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import www.antholim.co.Backend.models.User;
+import www.antholim.co.Backend.services.UserService;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl implements UserService {
+    private final AuthenticationManager authenticationManager;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Override
+    public boolean verifyToken(String token) {
+        return false;
+    }
+    private User createUser(String username, String email, String password) {
+        User user = new User();
+        user.setUsername(username).setPassword(bCryptPasswordEncoder.encode(password)).setEmail(email);
+        return user;
+    }
+    private void authenticate(String username, String password) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (BadCredentialsException e) {
+            log.error("Invalid credentials for user: {}", username, e);
+//            throw Exception(USER, CUSTOM_EXCEPTION, "Wrong credentials.");
+        } catch (DisabledException e) {
+            log.error("Authentication failed, account is disabled: {}", username, e);
+//            throw new Exception("Account is disabled.");
+        } catch (Exception e) {
+            log.error("Authentication failed for user: {}", username, e);
+//            throw new Exception("Authentication failed.");
+        }
+    }
+}
