caf102f566101d0689d2372a1612e39bf0092fb9
antholim <139510082+antholim@users.noreply.github.com>
2025-04-14 22:29:29 -0400

    spring security

---
+++ Backend/src/main/java/www/antholim/co/Backend/models/User.java
@@ -0,0 +1,30 @@
+package www.antholim.co.Backend.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class User implements UserDetails {
+    private String _id;
+    private String username;
+    private String password;
+    private String email;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
---
+++ Backend/src/main/java/www/antholim/co/Backend/services/UserService.java
@@ -0,0 +1,5 @@
+package www.antholim.co.Backend.services;
+
+public interface UserService {
+    boolean verifyToken(String token);
+}
