158a22a8ad35ec556afaac4d2eba1ec44244562a
antholim <139510082+antholim@users.noreply.github.com>
2025-05-30 21:28:22 -0400

    changes

--- Backend/src/main/java/www/antholim/co/Backend/models/User.java
+++ Backend/src/main/java/www/antholim/co/Backend/models/User.java
@@ -1,26 +1,28 @@
 package www.antholim.co.Backend.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.lang.annotation.Documented;
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Accessors(chain = true)
 @Setter
 @Getter
-@Table(name = "user")
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
-    private String _id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
     private String username;
     private String password;
     private String email;
